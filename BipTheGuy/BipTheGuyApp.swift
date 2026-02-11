995ac82ebf860efad4e6ec58770b8daa4ea95bc1
Jorge J. Caraballo <jcaraballo79@gmail.com>
2026-02-10 22:39:01 -0500

    Initial Commit

---
+++ BipTheGuy/BipTheGuyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BipTheGuyApp.swift
+//  BipTheGuy
+//
+//  Created by Jorge Caraballo on 2/10/26.
+//
+
+import SwiftUI
+
+@main
+struct BipTheGuyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
